5c68b3e00b3035ba18e7cb8755228ee6f9b0fbfa
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-02-03 13:50:03 -0800

    Deleted bionic-workarounds.c and .h
    I have no intention to call into Java to get the locale because it is a lot of
    work for something that will probably be rewritten anyway

--- jni/clips/bionic-workarounds.c
+++
@@ -1,8 +0,0 @@
-#include <jni.h>
-#include "clips.h"
-#include "bionic-workarounds.h"
-
-char* bionic_setlocale(void* theEnv, int value, char* input) {
-	JNIEnv* env = (JNIEnv*)GetEnvironmentContext(theEnv);	
-		
-}
