b33b45f45edc4bdae322d2ac6671eaf1ae673290
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-02-03 12:56:43 -0800

    Changed the License of bionic-workarounds.h
    I also added some more comments as well as modifying the header for
    bionic_setlocale to take in a void pointer representing the environment which
    contains the JNIEnv pointer in the "Context" slot.

--- jni/workarounds/bionic/bionic-workarounds.h
+++ jni/workarounds/bionic/bionic-workarounds.h
@@ -1,35 +1,32 @@
 /*
-Copyright (c) 2013, Joshua Scoggins 
-All rights reserved.
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in 
+the Software without restriction, including without limitation the rights to 
+use, copy, modify, merge, publish, distribute, and/or sell copies of the 
+Software, and to permit persons to whom the Software is furnished to do so.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of Joshua Scoggins nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF THIRD PARTY RIGHTS. IN NO EVENT 
+SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, OR ANY SPECIAL INDIRECT OR 
+CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, 
+DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS 
+SOFTWARE.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL Joshua Scoggins BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-
+/*
+ * Written by Joshua Scoggins 
+ * Started on February 3, 2013
+ *
+ * Contains workaround methods for features missing from Android's bionic
+ */
 #ifndef _BIONIC_WORKAROUNDS_H
 #define _BIONIC_WORKAROUNDS_H
 /* 
  * Calls the Java version of setlocale. This is necessary since bionic doesn't
- * actually implement setlocale
+ * actually implement setlocale. We use the environment as a way to get the JNI
+ * context out so it's usable.
  */
-char* bionic_setlocale(int value, char* value);
+char* bionic_setlocale(void* theEnv, int value, char* value);
 #endif
