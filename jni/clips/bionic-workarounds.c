078c9aaa1485e10f0a3d66586e50220952c55444
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-02-03 13:42:53 -0800

    Started implementing bionic-workarounds.c
    I have started implementing bionic_setlocale

--- jni/clips/bionic-workarounds.c
+++ jni/clips/bionic-workarounds.c
@@ -1,3 +1,8 @@
 #include <jni.h>
 #include "clips.h"
 #include "bionic-workarounds.h"
+
+char* bionic_setlocale(void* theEnv, int value, char* input) {
+	JNIEnv* env = (JNIEnv*)GetEnvironmentContext(theEnv);	
+		
+}
